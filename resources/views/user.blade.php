bdf9950614248fb590ce4534ffe056a40d1ae734
Anthea05 <syaff.atthaillah@gmail.com>
2026-03-02 15:19:00 +0700

    alhamdulillah

---
+++ resources/views/user.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Data User</title>
+</head>
+
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
