57b7d80e735dec61a916231857ef6e44058ce9d2
Anthea05 <syaff.atthaillah@gmail.com>
2026-03-26 19:11:01 +0700

    2.5 done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-            ],
-        );
+        $user->username = 'manager12';
         $user->save();
 
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); // true [cite: 502]
+        $user->wasChanged('username'); // true [cite: 503]
+        $user->wasChanged(['username', 'level_id']); // true [cite: 504]
+        $user->wasChanged('nama'); // false [cite: 505]
+
+        dd($user->wasChanged(['nama', 'username'])); // true [cite: 506]
     }
 }
